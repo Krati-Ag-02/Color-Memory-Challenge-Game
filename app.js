88d02d492e535e4a21a8df45d634b6ff4b0025a5
Krati-Ag-02 <kratiag322004@gmail.com>
2025-05-12 18:07:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,81 @@
+let userseq=[];
+let gameseq=[];
+let level=0;
+let started=false;
+let btns=["red","pink","yellow","blue"];
+let highestscore=0;
+
+document.addEventListener("keypress",function(){
+    if(started==false){
+        
+        started=true;
+        levelup();
+    }
+});
+
+
+let h2=document.querySelector("h2");
+
+function levelup(){
+  userseq=[];
+    level++;
+    h2.innerText=`Level ${level}`;
+    let randidx=Math.floor(Math.random()*4);
+    let randcolor=btns[randidx];
+    let randbtn=document.querySelector(`.${randcolor}`);
+    gameseq.push(randcolor);
+    
+ btnflash(randbtn);
+}
+
+function btnflash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250);
+
+}
+function btnpress(){
+    let btn=this;
+    btnflash(btn);
+
+    let usercolor=btn.getAttribute("id");
+    userseq.push(usercolor);
+
+   
+    checkseq(userseq.length-1);
+
+}
+
+function checkseq(idx){
+   
+    if(gameseq[idx]==userseq[idx])
+    {
+        if(gameseq.length==userseq.length)
+            setTimeout(levelup,1000);
+    }
+    else{
+        if(highestscore<level)
+            highestscore=level;
+    h2.innerHTML=`Game Over!!<br>Your score was ${level}||||Highest score is ${highestscore}<br>Press any key to restart.`;
+    
+    document.querySelector("body").style.backgroundColor="red";
+    setTimeout(function(){
+        document.querySelector("body").style.backgroundColor="white";
+    },150)
+    reset();
+    }
+}
+
+
+let allbtn=document.querySelectorAll(".btn"); 
+for(btn of allbtn){
+    btn.addEventListener("click",btnpress);
+}
+
+function reset(){
+    started=false;
+    userseq=[];
+    gameseq=[];
+    level=0;
+}
